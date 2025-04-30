600d238f2a6e598b3ae7dc2cc05568ebc9a1cb33
Tinku kohad <127082464+kohadtinku@users.noreply.github.com>
2025-04-30 11:31:18 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -50,6 +50,14 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 app.use(
     fileUpload({
         useTempFiles: true,
